2e316b5444ea4e6a8c692a9fdd62776869b9e02a
Hartmann Ferenc <>
2017-11-09 11:33:07 +0100

    Redux system upgraded but still not ready

--- javascript/menu_controller.js
+++ javascript/menu_controller.js
@@ -2,79 +2,97 @@
 
 console.log('menu_controller.js file succesfully loaded');
 
+
 const mainAudio = document.getElementById('main-audio');
+let store = Redux.createStore(reducer);
+let gameFrame;
+let mainMenuMusicButtonDisabledid = false;
+
+store.subscribe(stateExecutor);
+
+function stateExecutor() {
+  render();
+  mainMusicController();
+}
 
 function reducer(state, action) {
   if (typeof state === 'undefined') {
-    state = {};
-    state.mainAudio = 'OFF';
-    state.currentPage = 'pre_menu';
+    state = {
+      mainMusic: MAINMUSIC_OFF,
+      currentPage: PREMENU_ACTIVE
+    };
     return state
   }
 
   switch (action.type) {
-    case 'MAINMENU':
-       state.currentPage = 'main_menu';
-       return state
-    case 'ON':
-       state.mainAudio = 'ON';
-       return state
-    case 'OFF':
-       state.mainAudio = 'OFF';
-       return state
+    case 'MAINMENU_ACTIVE':
+     return Object.assign({}, state, {
+              currentPage: action.type
+            })
+    case 'MAINMUSIC_ON':
+      return Object.assign({}, state, {
+               mainMusic: action.type,
+               currentMusic: action.src,
+             })
+    case 'MAINMUSIC_OFF':
+      return Object.assign({}, state, {
+               mainMusic: action.type
+             })
     default:
       return state
   }
 }
 
-
-var store = Redux.createStore(reducer);
-let gameFrame;
-let mainMenuMusicButtonDisabledid = false;
-
 document.getElementById('game-frame').onload = function() {
   gameFrame = document.getElementById("game-frame").contentWindow;
 
-  if(store.getState().currentPage == 'pre_menu') {
+  if(store.getState().currentPage == PREMENU_ACTIVE) {
    gameFrame.document.getElementById('pre-menu-play-buttonid')
       .addEventListener('click', function () {
-          store.dispatch({ type: 'MAINMENU' });
-          store.dispatch({ type: 'ON' });
+          store.dispatch({ type: MAINMENU_ACTIVE });
+          store.dispatch({ type: MAINMUSIC_ON, src: "../assets/sound/KRO_main_menu.mp3" });
           mainAudio.play();
     })
   }
 
-  if(store.getState().currentPage == 'main_menu') {
+  if(store.getState().currentPage == MAINMENU_ACTIVE) {
     gameFrame.document.getElementById('main-menu-music-buttonid')
       .addEventListener('click', function () {
-        if (store.getState().mainAudio == 'ON') {
-          store.dispatch({ type: 'OFF' });
-          mainAudio.pause();
-          mainAudio.currentTime = 0;
+        if (store.getState().mainMusic == MAINMUSIC_ON) {
+          store.dispatch({ type: MAINMUSIC_OFF });
           return
         }
-        if (store.getState().mainAudio == 'OFF') {
-          store.dispatch({ type: 'ON' })
-          mainAudio.play();
+        if (store.getState().mainMusic == MAINMUSIC_OFF) {
+          store.dispatch({ type: MAINMUSIC_ON, src: "../assets/sound/KRO_main_menu.mp3" });
           return
         }
       })
   }
 
-  if (store.getState().currentPage == 'main_menu') {
+  if (store.getState().currentPage == MAINMENU_ACTIVE) {
     mainMenuMusicButtonDisabledid = gameFrame.document.getElementById('main-menu-music-button-disabledid');
   }
 }
 
 function render() {
-  if (store.getState().mainAudio == 'OFF' && mainMenuMusicButtonDisabledid !== false) {
+  if (store.getState().mainMusic == MAINMUSIC_OFF && mainMenuMusicButtonDisabledid !== false) {
     mainMenuMusicButtonDisabledid.classList.add('main-menu-music-button-disabled');
   }
 
-  if (store.getState().mainAudio == 'ON' && mainMenuMusicButtonDisabledid !== false) {
+  if (store.getState().mainMusic == MAINMUSIC_ON && mainMenuMusicButtonDisabledid !== false) {
     mainMenuMusicButtonDisabledid.classList.remove('main-menu-music-button-disabled');
   }
 }
 
-store.subscribe(render)
-setInterval(function () {console.log(store.getState())}, 1000)
+function mainMusicController() {
+  if (store.getState().currentMusic && store.getState().mainMusic == MAINMUSIC_ON) {
+    mainAudio.setAttribute("src", store.getState().currentMusic);
+    mainAudio.play();
+  }
+  if (store.getState().mainMusic == MAINMUSIC_OFF) {
+    mainAudio.pause();
+    mainAudio.currentTime = 0;
+  }
+}
+
+setInterval(function () {console.log(store.getState())}, 1000);
